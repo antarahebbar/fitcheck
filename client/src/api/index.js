96e5fba323a7e2c9881f7240e8e3bbe9fe97ac3f
ahebbar <ahebbar@umich.edu>
2023-08-14 22:10:49 -0400

    server deployed via heroku

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL : 'http://localhost:4000' });
+const api = axios.create({ baseURL : 'https://fitcheck-fa5fc3616e23.herokuapp.com/' });
 
 api.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')){
